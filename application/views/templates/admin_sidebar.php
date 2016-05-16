290f05ea1683e4b63760b16b49a74a2d6f7ffc98
tomato4jo <jihoonpark.la@gmail.com>
2016-05-16 17:30:38 +1200

    recommit changes on admin panel side bar

--- application/views/templates/admin_sidebar.php
+++ application/views/templates/admin_sidebar.php
@@ -1,6 +1,10 @@
-<div class="col-sm-3" style="padding-top:20px;background:#EEEEEE;">
+<?php
+$CI = &get_instance();
+?>
+
+<div class="col-sm-3" style="padding:20px;background:#EEEEEE;">
   <ul class="nav nav-pills nav-stacked">
-    <li role="presentation" class="active"><a href="<?php echo site_url('Admin/dashboard'); ?>">Dashboard</a></li>
-    <li role="presentation"><a href="<?php echo site_url('Admin/user'); ?>">Users</a></li>
+    <li role="presentation"<?php echo $CI->utils->uriMatch('Admin', 'dashboard') ? 'class="active"' : ''; ?>><a href="<?php echo site_url('Admin/dashboard'); ?>">Dashboard</a></li>
+    <li role="presentation"<?php echo $CI->utils->uriMatch('Admin', 'user') ? 'class="active"' : ''; ?>><a href="<?php echo site_url('Admin/user'); ?>">Users</a></li>
   </ul>
 </div>
