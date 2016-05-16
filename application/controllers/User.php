6dcb73179303732353925f00d4aef245298ad8db
Chad <haoduan@outlook.com>
2016-05-16 16:30:30 +1200

    update @ 4:30PM 16/05/2016

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -17,27 +17,42 @@ class User extends CI_Controller
 
     public function signupProcess()
     {
-        $user['fname'] = ucfirst(strtolower($_POST['inputFirstname']));
-        $user['lname'] = ucfirst(strtolower($_POST['inputLastname']));
-        $user['email'] = strtolower($_POST['inputEmail']);
-        $user['password'] = md5($_POST['inputPassword']);
-        $user['is_admin'] = 0;
-
-        if ($this->UserModel->addUser($user) === true) {
-            if (!isset($_SESSION)) {
-                session_start();
-            } else {
-                $_SESSION = array();
-            }
-            $_SESSION['user_loggedin'] = true;
-            $_SESSION['user'] = $this->UserModel->getUser($user['email']);
-            header('Location: '.base_url());
-            exit();
-        } else {
+        $result = $this->validataSignupForm();
+        if (!empty($result)) {
             $view_data['heading'] = 'Oops! An error occurred...';
-            $view_data['message'] = '<p>We cannot create your account.<br>It is probably because the email address you entered already exists.</p>';
+            $view_data['message'] = '<p>'.$result.'</p>';
             $this->utils->view('errors/html/error_general', 'Hack A Space', $view_data);
+        } else {
+            $user['fname'] = ucfirst(strtolower($_POST['inputFirstname']));
+            $user['lname'] = ucfirst(strtolower($_POST['inputLastname']));
+            $user['email'] = strtolower($_POST['inputEmail']);
+            $user['password'] = md5($_POST['inputPassword']);
+            $user['is_admin'] = 0;
+
+            if ($this->UserModel->addUser($user) === true) {
+                if (!isset($_SESSION)) {
+                    session_start();
+                } else {
+                    $_SESSION = array();
+                }
+                $_SESSION['user_loggedin'] = true;
+                $_SESSION['user'] = $this->UserModel->getUser($user['email']);
+                header('Location: '.base_url());
+                exit();
+            } else {
+                $view_data['heading'] = 'Oops! An error occurred...';
+                $view_data['message'] = '<p>We cannot create your account.<br>It is probably because the email address you entered already exists.</p>';
+                $this->utils->view('errors/html/error_general', 'Hack A Space', $view_data);
+            }
         }
+    }
+
+    private function validataSignupForm()
+    {
+        $msg = !filter_var(strtolower($_POST['inputEmail']), FILTER_VALIDATE_EMAIL) ? 'Email address is invalid!<br>' : '';
+        $msg = $msg.(md5($_POST['inputPassword']) != md5($_POST['inputConfirmPassword']) ? 'Password does not match confirm password!<br>' : '');
+
+        return $msg;
     }
 
     public function login()
