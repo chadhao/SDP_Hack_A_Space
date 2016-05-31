d8d3d89d7bea736368aaba621485da0249277574
Chanui123 <eteroalafaele080@gmail.com>
2016-06-01 10:08:16 +1200

    changes on ListingModel

--- application/models/ListingModel.php
+++ application/models/ListingModel.php
@@ -3,5 +3,13 @@
 class ListingModel extends CI_Model
 {
     private static $table_name = 'listings';
-    
+
+    public function getAllListing()
+    {
+        return $this->db->get(self::$table_name)->result();
+    }
+
+    public function getListing($lid)
+    {
+    }
 }
